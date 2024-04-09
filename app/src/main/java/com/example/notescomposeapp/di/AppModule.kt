b00119173aac1c5e55364d6f550ff89037c031eb
aiswarya-jayadas <aiswarya.jayadas@qburst.com>
2024-04-09 15:32:39 +0530

    latest  commit

---
+++ app/src/main/java/com/example/notescomposeapp/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.example.notescomposeapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.notescomposeapp.data.NoteDao
+import com.example.notescomposeapp.data.NoteDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(context: Context): NoteDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            NoteDatabase::class.java,
+            "notes.db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteDao(noteDatabase: NoteDatabase): NoteDao {
+        return noteDatabase.dao
+    }
+}
