8b417a07deabfa2d62986b722af595e2f6bf49c6
aiswarya-jayadas <aiswarya.jayadas@qburst.com>
2024-04-11 06:40:01 +0530

    updated hilt version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,11 @@ dependencies {
     // hilt
     implementation("com.google.dagger:hilt-android:2.50")
     ksp("com.google.dagger:hilt-android-compiler:2.50")
+    ksp("androidx.hilt:hilt-compiler:1.2.0")
+
+    // hit - navigation
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.hilt:hilt-navigation:1.2.0")
 
 
 
--- app/src/main/java/com/example/notescomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/notescomposeapp/MainActivity.kt
@@ -3,37 +3,37 @@ package com.example.notescomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.room.Room
-import com.example.notescomposeapp.data.NoteDatabase
 import com.example.notescomposeapp.presentation.AddNoteScreen
 import com.example.notescomposeapp.presentation.NoteScreen
 import com.example.notescomposeapp.presentation.NoteState
 import com.example.notescomposeapp.presentation.NoteViewModel
 import com.example.notescomposeapp.ui.theme.NotesComposeAppTheme
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import androidx.lifecycle.ViewModelProvider
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var mainViewModel: NoteViewModel
 
-    private val viewModel: NoteViewModel by viewModels()
+
+
 
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mainViewModel = ViewModelProvider(this)[NoteViewModel::class.java]
+
         setContent {
             NotesComposeAppTheme {
-                val state by viewModel.state.collectAsState(initial = NoteState())
+                val state by mainViewModel.state.collectAsState(initial = NoteState())
 
                 val navController = rememberNavController()
                 NavHost(navController = navController, startDestination = "NotesScreen") {
@@ -42,7 +42,7 @@ class MainActivity : ComponentActivity() {
                         NoteScreen(
                             state = state,
                             navController = navController,
-                            onEvent = viewModel::onEvent
+                            onEvent = mainViewModel::onEvent
                         )
                     }
 
@@ -50,7 +50,7 @@ class MainActivity : ComponentActivity() {
                         AddNoteScreen(
                             state = state,
                             navController = navController,
-                            onEvent = viewModel::onEvent
+                            onEvent = mainViewModel::onEvent
                         )
                     }
 
